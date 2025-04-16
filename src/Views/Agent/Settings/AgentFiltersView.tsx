56c4320bbfac84c05478fb6f4779e1c38fae77f4
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-16 18:17:45 +0300

    Update AgentFiltersView.tsx

--- src/Views/Agent/Settings/AgentFiltersView.tsx
+++ src/Views/Agent/Settings/AgentFiltersView.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 import { BoxView } from '../../../Components/Views/BoxView';
 import { ActionType } from '../../../Models/Actions';
 import { BaseCheckbox } from '../../../Components/Checkboxes/BaseCheckbox';
-import { normalizeActionType } from '../../../Services/Actions';
+import { ACTION_TYPES_BY_GROUP, normalizeActionType } from '../../../Services/Actions';
 import { AgentFilters } from '../../../Models/Agent';
 import { FC } from 'react';
 
@@ -15,9 +15,22 @@ export const AgentFiltersView: FC<Props> = ({ filters, setFilters }) => {
   const onAction = (type: ActionType) => () => {
     setFilters({
       ...filters,
-      actions: filters.actions.includes(type) ? filters.actions.filter((t) => t !== type) : [...filters.actions, type]
+      actions: filters.actions.includes(type)
+        ? filters.actions.filter((action) => action !== type)
+        : [...filters.actions, type]
     });
   };
+
+  const onActions = (types: ActionType[]) => () => {
+    setFilters({
+      ...filters,
+      actions: isActionsChecked(types)
+        ? filters.actions.filter((action) => !types.includes(action)) // снять группу
+        : [...filters.actions, ...types] // добавить группу
+    });
+  };
+
+  const isActionsChecked = (types: ActionType[]) => types.every((action) => filters.actions.includes(action));
 
   return (
     <Box>
@@ -30,6 +43,23 @@ export const AgentFiltersView: FC<Props> = ({ filters, setFilters }) => {
             onChange={onAction(action)}
           />
         ))}
+      </BoxView>
+      <BoxView title={'Actions group filters'}>
+        <BaseCheckbox
+          label={'Input'}
+          checked={isActionsChecked(ACTION_TYPES_BY_GROUP.input)}
+          onChange={onActions(ACTION_TYPES_BY_GROUP.input)}
+        />
+        <BaseCheckbox
+          label={'Action'}
+          checked={isActionsChecked(ACTION_TYPES_BY_GROUP.action)}
+          onChange={onActions(ACTION_TYPES_BY_GROUP.action)}
+        />
+        <BaseCheckbox
+          label={'Assert'}
+          checked={isActionsChecked(ACTION_TYPES_BY_GROUP.assert)}
+          onChange={onActions(ACTION_TYPES_BY_GROUP.assert)}
+        />
       </BoxView>
     </Box>
   );
