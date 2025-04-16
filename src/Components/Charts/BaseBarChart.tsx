b39f85bef1b05c3a7658dad7fc88964aee0dabdf
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-16 10:38:03 +0300

    Update BaseBarChart.tsx

--- src/Components/Charts/BaseBarChart.tsx
+++ src/Components/Charts/BaseBarChart.tsx
@@ -10,6 +10,7 @@ import {
 } from '@mui/x-charts';
 import { AxisValueFormatterContext } from '@mui/x-charts/internals';
 import { DatasetType } from '@mui/x-charts/models/seriesType/config';
+import { useTheme } from '@mui/material';
 
 interface BarChartYAxis {
   data?: (null | number)[];
@@ -33,13 +34,20 @@ type BaseLineChartProps<T> = {
 };
 
 export const BaseBarChart = <T,>({ xAxis, yAxis, dataset }: BaseLineChartProps<T>) => {
+  const theme = useTheme();
+
   return (
     <ResponsiveChartContainer xAxis={xAxis} series={yAxis.map((axis) => ({ ...axis, type: 'bar' }))} dataset={dataset}>
       <BarPlot />
       <ChartsGrid vertical={true} horizontal={true} />
       <ChartsXAxis />
       <ChartsYAxis />
-      <ChartsLegend />
+      <ChartsLegend
+        itemMarkHeight={13}
+        itemMarkWidth={13}
+        position={{ horizontal: 'middle', vertical: 'top' }}
+        labelStyle={{ fontSize: theme.typography.subtitle2.fontSize }}
+      />
       <ChartsTooltip />
       <ChartsAxisHighlight x={'band'} />
     </ResponsiveChartContainer>
