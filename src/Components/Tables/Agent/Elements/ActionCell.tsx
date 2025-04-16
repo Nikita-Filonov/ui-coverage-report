099dceff8b661b3e2d05603e71d717f62579e16a
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-16 18:16:59 +0300

    Update ActionCell.tsx

--- src/Components/Tables/Agent/Elements/ActionCell.tsx
+++ src/Components/Tables/Agent/Elements/ActionCell.tsx
@@ -3,8 +3,12 @@ import { FC, ReactNode } from 'react';
 import { ActionType } from '../../../../Models/Actions';
 import MouseOutlinedIcon from '@mui/icons-material/MouseOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+import DoNotTouchOutlinedIcon from '@mui/icons-material/DoNotTouchOutlined';
+import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
-import TextSnippetOutlinedIcon from '@mui/icons-material/TextSnippetOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 import { normalizeActionType } from '../../../../Services/Actions';
 
 type Props = {
@@ -12,10 +16,24 @@ type Props = {
 };
 
 const MAP_ACTION_TYPE_TO_ICON: Record<ActionType, ReactNode> = {
+  // input
   [ActionType.Fill]: <EditOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Type]: <EditOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Select]: <EditOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+
+  // action
   [ActionType.Click]: <MouseOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
-  [ActionType.AssertVisible]: <VisibilityOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
-  [ActionType.AssertHaveText]: <TextSnippetOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />
+  [ActionType.Hover]: <MouseOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+
+  // assert
+  [ActionType.Text]: <VisibilityOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Value]: <VisibilityOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Hidden]: <VisibilityOffOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Visible]: <VisibilityOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Checked]: <CheckBoxOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Enabled]: <ToggleOnIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Disabled]: <DoNotTouchOutlinedIcon sx={{ mr: 1.5 }} fontSize={'small'} />,
+  [ActionType.Unchecked]: <CheckBoxOutlineBlankIcon sx={{ mr: 1.5 }} fontSize={'small'} />
 };
 
 export const ActionCell: FC<Props> = ({ type }) => {
