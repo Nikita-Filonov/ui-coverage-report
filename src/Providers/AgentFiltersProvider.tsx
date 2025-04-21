b2d8daf2b0140b652f6d4ce5fc69075c6d8e2450
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-21 14:07:14 +0300

    Update AgentFiltersProvider.tsx

--- src/Providers/AgentFiltersProvider.tsx
+++ src/Providers/AgentFiltersProvider.tsx
@@ -1,6 +1,7 @@
-import React, { Dispatch, FC, PropsWithChildren, SetStateAction, useContext, useState } from 'react';
+import { createContext, FC, PropsWithChildren, useContext, useState } from 'react';
 import { AgentFilters } from '../Models/Agent';
 import { ActionType } from '../Models/Actions';
+import { loadFromStorage, saveIntoStorage, StorageKey } from '../Services/Storage';
 
 const DEFAULT_AGENT_FILTERS: AgentFilters = {
   actions: Object.values(ActionType)
@@ -8,14 +9,21 @@ const DEFAULT_AGENT_FILTERS: AgentFilters = {
 
 export type AgentFiltersContextProps = {
   filters: AgentFilters;
-  setFilters: Dispatch<SetStateAction<AgentFilters>>;
+  setFilters: (filters: AgentFilters) => void;
   clearAllFilters: () => void;
 };
 
-const AgentFiltersContext = React.createContext<AgentFiltersContextProps | null>(null);
+const AgentFiltersContext = createContext<AgentFiltersContextProps | null>(null);
 
 const AgentFiltersProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [filters, setFilters] = useState<AgentFilters>(DEFAULT_AGENT_FILTERS);
+  const [filters, setFiltersInternal] = useState<AgentFilters>(
+    loadFromStorage({ key: StorageKey.AgentFilters, fallback: DEFAULT_AGENT_FILTERS })
+  );
+
+  const setFilters = (filters: AgentFilters) => {
+    setFiltersInternal(filters);
+    saveIntoStorage({ key: StorageKey.AgentFilters, data: filters });
+  };
 
   const clearAllFilters = () => setFilters(DEFAULT_AGENT_FILTERS);
 
