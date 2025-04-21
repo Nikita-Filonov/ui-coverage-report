be9ba3ea4098b784f5774a470facc99d955a01c8
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-21 14:08:51 +0300

    Update Agent.ts

--- src/Services/Agent.ts
+++ src/Services/Agent.ts
@@ -4,6 +4,7 @@ import { RefObject, useEffect } from 'react';
 import { useAgentFilters } from '../Providers/AgentFiltersProvider';
 import { useAgentSettings } from '../Providers/AgentSettingsProvider';
 import { useInitialState } from '../Providers/InitialStateProvider';
+import { useTheme } from '../Providers/ThemeProvider';
 
 type UseAgentActionsProps = {
   frameRef: RefObject<HTMLIFrameElement | null>;
@@ -12,7 +13,12 @@ type UseAgentActionsProps = {
 export const useAgentActions = ({ frameRef }: UseAgentActionsProps) => {
   const { filters } = useAgentFilters();
   const { settings } = useAgentSettings();
+  const { themeMode } = useTheme();
   const { appCoverage } = useInitialState();
+
+  useEffect(() => {
+    onSyncAgent();
+  }, [filters, settings, themeMode]);
 
   const postMessage = (state: AgentState) => {
     const frameWindow = frameRef?.current?.contentWindow;
@@ -22,17 +28,11 @@ export const useAgentActions = ({ frameRef }: UseAgentActionsProps) => {
   };
 
   const onSyncAgent = () => {
-    postMessage({
-      settings,
-      elements: filterElementCoverageByActions({ elements: appCoverage.elements, actions: filters.actions })
-    });
+    const elements = filterElementCoverageByActions({ elements: appCoverage.elements, actions: filters.actions });
+    postMessage({ settings, elements, themeMode });
   };
 
-  const onClearAgent = () => postMessage({ settings, elements: [] });
-
-  useEffect(() => {
-    onSyncAgent();
-  }, [filters, settings]);
+  const onClearAgent = () => postMessage({ settings, elements: [], themeMode });
 
   return { onSyncAgent, onClearAgent };
 };
